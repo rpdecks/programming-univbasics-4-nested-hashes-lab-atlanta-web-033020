6c51ef6bb99d23ba38b4d74821618d1ec04ab9ef
Robert Phillips <rpdecks@gmail.com>
2020-03-13 13:51:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,8 +80,9 @@ def adding_matz
         :languages => ["C"]
       }
     }
-programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+#programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+#programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+programmer_hash
 programmer_hash
 end
 
